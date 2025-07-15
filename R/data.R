ab197897688754e80329a00c56fe042cd72dda37
Ruben Almey <94168719+rualmey@users.noreply.github.com>
2025-07-15 09:53:03 +0000

    Add benchmark dataset

--- R/data.R
+++ R/data.R
@@ -7,12 +7,36 @@
 #'   listed.
 #' @export
 #' @examples
-#' hptmUsage_data()
-#' hptmUsage_data("H3.fasta")
-hptmUsage_data <- function(file = NULL) {
+#' hptmUsageData()
+#' hptmUsageData("H3.fasta")
+hptmUsageData <- function(file = NULL) {
   if (is.null(file)) {
     dir(fs::path_package("extdata", package = "hptmUsage"))
   } else {
     fs::path_package("extdata", file, package = "hptmUsage")
   }
 }
+
+#' hPTM Benchmark Data
+#'
+#' A preprocessed `QFeatures` of hPTM data, functioning as a benchmark for
+#' downstream analysis and statistics. The samples consist of commercial histone
+#' extracts treated with a histone deacetylase (HDAC1) in a time-lapse design as
+#' described in the reference below.
+#'
+#' @format TODO A QFeatures with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{assay}{contains the raw peptide intensities}
+#'   \item{rowData}{contains a variable "Proteins" with the protein accession and an variable ecoli to indicate if the protein is a spikin}
+#'   \item{colData}{contains a factor condition indicating the spike-in condition}
+#'   ...
+#' }
+#' @source <https://www.ebi.ac.uk/pride/archive/projects/PXD009910>
+#' @references Clerck, L. D.; Willems, S.; Daled, S.; Puyvelde, B. V.;
+#'   Verhelst, S.; Corveleyn, L.; Deforce, D.; Dhaenens, M. An Experimental
+#'   Design to Extract More Information from MS-Based Histone Studies. Molecular
+#'   Omics 2021, 17 (6), 929–938. <https://doi.org/10.1039/D1MO00201E>.
+#' @examples
+#' hPTM_benchmark |>
+#'   show()
+"hPTM_benchmark"
--- data-raw/hPTM_benchmark.R
+++ data-raw/hPTM_benchmark.R
@@ -1,22 +1,4 @@
-#' World Health Organization TB data
-#'
-#' A subset of data from the World Health Organization Global Tuberculosis
-#' Report ...
-#'
-#' @format ## `who`
-#' A data frame with 7,240 rows and 60 columns:
-#' \describe{
-#'   \item{country}{Country name}
-#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
-#'   \item{year}{Year}
-#'   ...
-#' }
-#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
-
-
-
-
-
+# Peptide ion export from the Progenesis QIP archive at https://www.ebi.ac.uk/pride/archive/projects/PXD009910
 hPTM_benchmark <- readProgenesis("./data-raw/hPTM_benchmark.csv")
 
 usethis::use_data(hPTM_benchmark, overwrite = TRUE)
