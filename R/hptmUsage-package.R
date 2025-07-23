4faf9ccef6c835e1d45551c852077770a7a7f4cd
Ruben Almey <94168719+rualmey@users.noreply.github.com>
2025-07-23 16:10:26 +0000

    Implement histone alignment

--- R/hptmUsage-package.R
+++ R/hptmUsage-package.R
@@ -7,7 +7,7 @@
 #' @importFrom dplyr across
 #' @importFrom dplyr mutate
 #' @importFrom methods as
-#' @importFrom rlang .data
+#' @importFrom rlang .data %||%
 #' @importMethodsFrom SummarizedExperiment rowData rowData<- colData colData<-
 ## usethis namespace: end
 setClassUnion("QFeatures_OR_SummarizedExperiment", c("QFeatures", "SummarizedExperiment"))
