7954ef41409cea487ee34cdf954f4333c99433c2
Ruben Almey <94168719+rualmey@users.noreply.github.com>
2025-11-30 16:13:32 +0000

    Implement usage calculation

---
+++ R/calculateUsage.R
@@ -0,0 +1,182 @@
+#' Calculate hPTM Usage
+#'
+#' @description
+#' hPTM usage is calculated by first normalizing histone peptidoforms (i.e.,
+#' subtracting the "parent protein" abundance) and then summarizing to the
+#' hPTM level.
+#'
+#' @param object The `QFeatures` or `SummarizedExperiment` object from which
+#'   hPTM usage needs to be calculated.
+#' @param ... Additional arguments passed to specific methods.
+#' @param usage_level A `character(1)` defining the level at which usage will be
+#'    calculated, i.e., what is considered the "parent protein". Must be one of:
+#'   * `"histone"` = the total histone abundance (default)
+#'   * `"histone_family"` = the total family abundance
+#'   * `"histone_group"` = the histone group abundance
+#' @param deconv A `character(1)` pointing to the rowData column defining the
+#'   features to deconvolute; for example, hPTMs after alignment mapping.
+#' @param sep The `character(1)` used to separate different hPTMs/... in the
+#'   `deconv` column.
+#' @param group A `character(1)` pointing to the rowData column defining the
+#'   level at which deconvolution features (e.g., hPTMs) will be grouped; for
+#'   example, the histone family or histone variant group.
+#' @returns
+#' A `QFeatures` with PTM usage assay(s) added or a PTM usage
+#' `SummarizedExperiment` (same as supplied).
+#' @export
+#' @references
+#' (1) Demeulemeester N, Gébelin M, Gomes LC, Lingor P, Carapito C, Martens L,
+#' Clement L. msqrob2PTM: Differential Abundance and Differential Usage Analysis
+#' of MS-Based Proteomics Data at the Posttranslational Modification and
+#' Peptidoform Level. Molecular & Cellular Proteomics. 2024 Feb 1;23(2):100708.
+#' <https://pubs.acs.org/doi/full/10.1021/acs.jproteome.2c00145>.
+#' @name calculateUsage
+setGeneric(
+  "calculateUsage",
+  function(
+    object,
+    ...,
+    usage_level = c("histone", "histone_family", "histone_group"),
+    deconv = "mods_ref",
+    sep = ";",
+    group = "histone_group"
+  ) {
+    standardGeneric("calculateUsage")
+  },
+  signature = "object"
+)
+
+#' @rdname calculateUsage
+#' @param i The index (`integer()`) or name (`character()`) of the assay(s) to
+#'   be processed.
+#' @param name Name(s) of the new assay(s) to add to the QFeatures object. Must
+#'   have the same length as i.
+#' @param identifier A `character(1)` naming a rowData variable uniquely
+#'   defining the features.
+#' @param ... Additional arguments passed to [QFeatures::aggregateFeatures()].
+#' @examples
+#' \dontrun{
+#' # By default, the entire chromatosome abundance (i.e., aggregated value of
+#' # all histone features) is used as normalization factor
+#' new_qf <- hptm_benchmark |>
+#'   calculateUsage(i = "precursorHistone")
+#' #' # A hPTM will be defined as the unique combination of histone
+#' # group and PTM, e.g., H33K27Me3
+#' rowData(new_qf[["ptm"]])[258, ]
+#' # but if two hPTMs are only quantified from the same features, then these
+#' # will form one "hPTM group".
+#' rowData(new_qf[["ptm"]])[257, ]
+#'
+#' # It is also possible to correct for the histone family abundance
+#' new_qf <- new_qf |>
+#'   calculateUsage(i = "precursorHistone", name = "ptm_family", usage_level = "histone_family") |>
+#' # or to do a "variant-corrected" analysis
+#'   calculateUsage(i = "precursorHistone", name = "ptm_group", usage_level = "histone_group")
+#'
+#' # The default aggregation function (`MsCoreUtils::robustSummary()`) can be
+#' # slow if feature groups are large, for example when `usage_level = "histone`
+#' # In this case, it can be faster to use `MsCoreUtils::medianPolish()` instead
+#' new_qf_mp <- hptm_benchmark |>
+#'   calculateUsage(
+#'     i = "precursorHistone",
+#'     fun = MsCoreUtils::medianPolish,
+#'     na.rm = TRUE
+#'   )
+#'
+#' # Finally, if the unaligned amino acid index is to be used instead
+#' new_qf_unaligned <- hptm_benchmark |>
+#'   calculateUsage(i = "precursorHistone", deconv = "mods_var")
+#'}
+setMethod(
+  "calculateUsage",
+  "QFeatures",
+  function(
+    object,
+    i,
+    name = "ptm",
+    identifier = "feature_number",
+    ...,
+    usage_level = c("histone", "histone_family", "histone_group"),
+    deconv = "mods_ref",
+    sep = ";",
+    group = "histone_group"
+  ) {
+    # Check arguments
+    i <- QFeatures:::.normIndex(object, i)
+    stopifnot(length(i) == length(name))
+    usage_level <- match.arg(usage_level)
+
+    object |>
+      normalizeUsage(
+        i = i,
+        name = paste0(i, "Norm", stringr::str_to_sentence(usage_level)),
+        usage_level = usage_level,
+        ...
+      ) |>
+      deconvolute(
+        i = paste0(i, "Norm", stringr::str_to_sentence(usage_level)),
+        name = paste0(i, "Norm", stringr::str_to_sentence(usage_level), "Deconv"),
+        identifier = identifier,
+        deconv = deconv,
+        sep = sep,
+        group = group
+      ) |>
+      QFeatures::aggregateFeatures(
+        i = paste0(i, "Norm", stringr::str_to_sentence(usage_level), "Deconv"),
+        fcol = "hptm",
+        name = name,
+        ...
+      )
+  }
+)
+
+#' @rdname calculateUsage
+#' @param ... Additional arguments passed to [QFeatures::aggregateFeatures()].
+#' @examples
+#' \dontrun{
+#' # By default, the entire chromatosome abundance (i.e., aggregated value of
+#' # all histone features) is used as normalization factor
+#' new_se <- hptm_benchmark[[5]] |>
+#'   calculateUsage()
+#' # A hPTM will be defined as the unique combination of histone
+#' # group and PTM, e.g., H33K27Unmod
+#' rowData(new_se)[258, ]
+#' # but if two hPTMs are only quantified from the same features, then these
+#' # will form one "hPTM group".
+#' rowData(new_se)[257, ]
+#'
+#' # It is also possible to correct for the histone family abundance
+#' new_se_hf <- hptm_benchmark[[5]] |>
+#'   calculateUsage(usage_level = "histone_family")
+#' # or to do a "variant-corrected" analysis
+#' new_se_vc <- hptm_benchmark[[5]] |>
+#'   calculateUsage(usage_level = "histone_group")
+#'
+#' # The default aggregation function (`MsCoreUtils::robustSummary()`) can be
+#' # slow if feature groups are large, for example when `usage_level = "histone`
+#' # In this case, it can be faster to use `MsCoreUtils::medianPolish()` instead
+#' new_se_mp <- hptm_benchmark[[5]] |>
+#'   calculateUsage(fun = MsCoreUtils::medianPolish, na.rm = TRUE)
+#'
+#' # Finally, if the unaligned amino acid index is to be used instead
+#' new_se_unaligned <- hptm_benchmark[[5]] |>
+#'   calculateUsage(deconv = "mods_var")
+#'}
+setMethod(
+  "calculateUsage",
+  "SummarizedExperiment",
+  function(
+    object,
+    ...,
+    usage_level = c("histone", "histone_family", "histone_group"),
+    deconv = "mods_ref",
+    sep = ";",
+    group = "histone_group"
+  ) {
+    object |>
+      normalizeUsage(usage_level = usage_level, ...) |>
+      deconvolute(deconv = deconv, sep = sep, group = group) |>
+      # consider MsCoreUtils::medianPolish() if fcol = histone?; this is faster and should perform similarly here
+      QFeatures::aggregateFeatures(fcol = "hptm", ...)
+  }
+)
---
+++ tests/testthat/test-calculateUsage.R
@@ -0,0 +1,208 @@
+# S4 Methods -------------------------------------------------------------------
+
+## calculateUsage.SummarizedExperiment -----------------------------------------
+
+test_that("calculateUsage,SE works with standard workflow", {
+  # Setup: Create an SE where two features will map to the same hPTM
+  mock_se <- new_mock_se(nrow = 4)
+  # Rows 1 and 2 will share the same hPTM
+  rowData(mock_se)$mods_ref <- c("K27Me3", "K27Me3", "K9Ac", "K36Me2")
+  rowData(mock_se)$histone_group <- c("H3", "H3", "H3", "H3")
+  rowData(mock_se)$histone <- TRUE
+
+  # Execution
+  # Using colMeans for aggregation to make verification easy
+  res_se <- calculateUsage(
+    mock_se,
+    usage_level = "histone",
+    fun = colMeans,
+    na.rm = TRUE
+  )
+
+  expect_s4_class(res_se, "SummarizedExperiment")
+
+  # Check Dimensions:
+  # Input 4 rows -> Normalization (4) -> Deconvolution (4) -> Aggregation
+  # hPTMs: "H3#K27Me3" (from rows 1&2), "H3#K9Ac" (row 3), "H3#K36Me2" (row 4)
+  # Result should have 3 rows
+  expect_equal(nrow(res_se), 3)
+
+  # Check Rownames
+  expect_true("H3#K27Me3" %in% rownames(res_se))
+
+  # Check Calculation Logic (Simplified):
+  # 1. Normalization: usage_level="histone".
+  #    Subtract mean of all histones (rows 1-4) from each value.
+  # 2. Aggregation: Group rows 1 and 2 (H3#K27Me3).
+  #    Value = Mean(NormValue1, NormValue2).
+
+  vals <- assay(mock_se)
+  # 1. Normalization factor (mean of all 4 rows per sample)
+  norm_factors <- colMeans(vals)
+  norm_vals <- sweep(vals, 2, norm_factors, "-")
+
+  # 2. Aggregation for H3#K27Me3 (rows 1 and 2)
+  expected_k27 <- colMeans(norm_vals[1:2, ])
+
+  expect_equal(assay(res_se)["H3#K27Me3", ], expected_k27)
+})
+
+test_that("calculateUsage,SE works with different usage_levels", {
+  mock_se <- new_mock_se(nrow = 3)
+  rowData(mock_se)$mods_ref <- c("K27Me3", "K27Me3", "K20Ac")
+  rowData(mock_se)$histone_family <- c("H3", "H3", "H4")
+  rowData(mock_se)$histone_group <- c("H3_V1", "H3_V1", "H4_V1")
+
+  # usage_level = "histone_family"
+  res_se <- calculateUsage(
+    mock_se,
+    usage_level = "histone_family",
+    fun = colMeans,
+    na.rm = TRUE
+  )
+
+  # Logic:
+  # H3 rows (1&2) normalized by mean of H3 rows.
+  # H4 row (3) normalized by itself (mean of 1 row).
+  # Aggregation: Rows 1&2 combine to H3_V1#K27Me3.
+
+  expect_equal(nrow(res_se), 2) # H3_V1#K27Me3 and H4_V1#K20Ac
+
+  # H4 check: Value - Mean(Value) = 0
+  expect_equal(as.vector(assay(res_se)["H4_V1#K20Ac", ]), rep(0, 7))
+})
+
+test_that("calculateUsage,SE passes arguments to internal functions", {
+  mock_se <- new_mock_se(nrow = 2)
+  rowData(mock_se)$alt_mods <- c("ModA|ModB", "ModA|ModB")
+  rowData(mock_se)$alt_group <- c("G1", "G1")
+
+  # Test custom deconv column, separator, grouping, and aggregation function
+  res_se <- calculateUsage(
+    mock_se,
+    deconv = "alt_mods",
+    sep = "|",
+    group = "alt_group",
+    usage_level = "histone"
+  )
+
+  expect_equal(nrow(res_se), 1)
+  expect_equal(rownames(res_se), "G1#ModA|ModB")
+})
+
+test_that("calculateUsage,SE handles empty inputs/outputs", {
+  # Case: Input has no valid mods for deconvolution
+  mock_se <- new_mock_se(nrow = 2)
+  rowData(mock_se)$mods_ref <- c("K27Me3", NA_character_)
+
+  # deconvolute filters out NAs, resulting in 1 row
+  res_se <- suppressWarnings(suppressMessages(calculateUsage(mock_se)))
+
+  expect_equal(nrow(res_se), 1)
+})
+
+## calculateUsage.QFeatures ----------------------------------------------------
+
+test_that("calculateUsage,QFeatures works with default parameters", {
+  mock_qf <- new_mock_qf(nrow = 4)
+  rowData(mock_qf[["assay1"]])$mods_ref <- c("K1", "K1", "K2", "K3")
+  rowData(mock_qf[["assay1"]])$histone_group <- "H3"
+  rowData(mock_qf[["assay1"]])$histone <- TRUE
+
+  res_qf <- calculateUsage(mock_qf, i = "assay1")
+
+  # Check if expected assays are added
+  # 1. Normalization: "assay1NormHistone"
+  # 2. Deconvolution: "assay1NormHistoneDeconv"
+  # 3. Aggregation (Result): "ptm" (default name)
+  expected_names <- c("assay1", "assay1NormHistone", "assay1NormHistoneDeconv", "ptm")
+  expect_true(all(expected_names %in% names(res_qf)))
+
+  # Check final assay dimensions
+  # "K1" rows combine -> 3 unique hPTMs
+  expect_equal(nrow(res_qf[["ptm"]]), 3)
+
+  # Check links chain
+  # assay1 -> assay1NormHistone
+  link1 <- assayLink(res_qf, "assay1NormHistone")
+  expect_equal(link1@from, "assay1")
+
+  # assay1NormHistone -> assay1NormHistoneDeconv
+  link2 <- assayLink(res_qf, "assay1NormHistoneDeconv")
+  expect_equal(link2@from, "assay1NormHistone")
+
+  # assay1NormHistoneDeconv -> ptm
+  link3 <- assayLink(res_qf, "ptm")
+  expect_equal(link3@from, "assay1NormHistoneDeconv")
+})
+
+test_that("calculateUsage,QFeatures works with custom names and multiple assays", {
+  mock_qf <- new_mock_qf(nrow = 2)
+  rowData(mock_qf[["assay1"]])$mods_ref <- c("K1", "K2")
+  rowData(mock_qf[["assay1"]])$histone_group <- "H3"
+  rowData(mock_qf[["assay1"]])$histone_family <- "H3"
+  rowData(mock_qf[["assay2"]])$mods_ref <- c("K1", "K2")
+  rowData(mock_qf[["assay2"]])$histone_group <- "H3"
+  rowData(mock_qf[["assay2"]])$histone_family <- "H3"
+
+  res_qf <- calculateUsage(
+    mock_qf,
+    i = c("assay1", "assay2"),
+    name = c("ptm1", "ptm2"),
+    usage_level = "histone_family"
+  )
+
+  # Check for intermediate names based on usage_level
+  # "assay1NormHistone_family", "assay1NormHistone_familyDeconv", "ptm1"
+  expect_true("assay1NormHistone_family" %in% names(res_qf))
+  expect_true("assay1NormHistone_familyDeconv" %in% names(res_qf))
+  expect_true("ptm1" %in% names(res_qf))
+  expect_true("ptm2" %in% names(res_qf))
+
+  expect_equal(nrow(res_qf[["ptm1"]]), 2)
+})
+
+test_that("calculateUsage,QFeatures passes arguments to inner methods", {
+  mock_qf <- new_mock_qf(nrow = 2)
+  rowData(mock_qf[["assay1"]])$my_mods <- c("A", "A")
+  rowData(mock_qf[["assay1"]])$my_grp <- c("G", "G")
+  rowData(mock_qf[["assay1"]])$my_id <- c("ID1", "ID2")
+
+  # Testing identifier passing and custom deconvolution args
+  res_qf <- calculateUsage(
+    mock_qf,
+    i = "assay1",
+    name = "result",
+    identifier = "my_id",
+    deconv = "my_mods",
+    group = "my_grp",
+    fun = colSums # Aggregation arg
+  )
+
+  # Check linkage uses correct identifier
+  # Link: assay1NormHistone -> assay1NormHistoneDeconv
+  # This link is created by deconvolute() which uses 'identifier'
+  link <- assayLink(res_qf, "assay1NormHistoneDeconv")
+  expect_equal(link@from, "assay1NormHistone")
+  expect_equal(link@fcol, "my_id")
+
+  # Check aggregation occurred (2 rows -> 1 row)
+  expect_equal(nrow(res_qf[["result"]]), 1)
+  expect_equal(rownames(res_qf[["result"]]), "G#A")
+})
+
+test_that("calculateUsage,QFeatures errors with mismatched arguments", {
+  mock_qf <- new_mock_qf()
+  expect_error(
+    calculateUsage(mock_qf, i = "assay1", name = c("n1", "n2")),
+    "length\\(i\\) == length\\(name\\) is not TRUE"
+  )
+})
+
+test_that("calculateUsage,QFeatures errors with invalid usage_level", {
+  mock_qf <- new_mock_qf()
+  expect_error(
+    calculateUsage(mock_qf, i = "assay1", usage_level = "invalid"),
+    "'arg' should be one of"
+  )
+})
